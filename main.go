84205dbc492931d74f4a099ed4876aedcf0600da
Guillaume J. Charmes <guillaume@charmes.net>
2019-08-10 22:58:42 -0400

    Initial postgres query for parts table
    Signed-off-by: Guillaume J. Charmes <guillaume@charmes.net>

--- main.go
+++ main.go
@@ -2,22 +2,72 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 
 	_ "github.com/lib/pq" // Load the postgres driver.
 )
 
+// TimeMetadata is the common time metadata fields for all components.
+type TimeMetadata struct {
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+	DeletedAt time.Time `db:"deleted_at"`
+}
+
+// Tag .
+type Tag struct {
+	ID   uuid.UUID
+	Name string
+	TimeMetadata
+}
+
+// // Location represents where part is stored.
+// type Location struct {
+// 	ID uuid.UUID
+
+// 	Name string
+// }
+
+// Part .
+type Part struct {
+	ID   uuid.UUID `db:"part_id"`
+	Name string    `db:"part_name"`
+
+	// Quantity int
+	// Location Location
+
+	//	Tags []Tag
+
+	TimeMetadata
+}
+
+var _ Part
+
 func main() {
-	db, err := sqlx.Connect("postgres", os.Getenv("PG_DSN"))
+	ctx := context.Background()
+	db, err := sqlx.ConnectContext(ctx, "postgres", os.Getenv("PG_DSN"))
 	if err != nil {
 		panic(err)
 	}
-	var now string
-	if err := db.GetContext(context.Background(), &now, "SELECT NOW()"); err != nil {
+
+	p := Part{ID: uuid.New(), Name: "boilerplate part!!"}
+	if _, err := db.NamedExecContext(ctx, `
+INSERT INTO parts
+(part_id, part_name, created_at, updated_at, deleted_at) VALUES
+(:part_id, :part_name, :created_at, :updated_at, :deleted_at)
+`, p); err != nil {
 		panic(err)
 	}
 
-	println("hello world!", now)
+	var p2 Part
+	if err := db.GetContext(ctx, &p2, db.Rebind("SELECT * FROM parts WHERE part_id = ? LIMIT 1"), p.ID); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("hello world!-->\n%s\n", p2.Name)
 }
--- migrations/20190810221522_initial_model.sql
+++ migrations/20190810221522_initial_model.sql
@@ -1,9 +1,19 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+
+CREATE TABLE parts (
+  part_id UUID NOT NULL PRIMARY KEY,
+
+  part_name VARCHAR NOT NULL,
+
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  deleted_at TIMESTAMP WITH TIME ZONE
+);
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+
 -- +goose StatementEnd
